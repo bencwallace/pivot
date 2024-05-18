de88575ff4dc3a6ee4c3582203038291d03dc216
Ben Wallace <bencwallace@gmail.com>
2024-05-18 11:53:41 +0200

    do not swallow clang-tidy exit code

--- tools/lint.sh
+++ tools/lint.sh
@@ -11,4 +11,4 @@ while getopts "v" opt; do
 done
 
 cppcheck --enable=warning,performance,information,missingInclude --suppress=missingIncludeSystem -i build --project=build/compile_commands.json
-find src \( -name *.cpp -o -name *.h \) -exec clang-tidy-14 -p build ${ARGS} {} \;
+find src \( -name *.cpp -o -name *.h \) -print0 | xargs -0 -n1 clang-tidy-14 -p build ${ARGS}
