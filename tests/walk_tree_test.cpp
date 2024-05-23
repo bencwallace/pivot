c8800d81705e3324bb704f5819b645da6795f51a
Ben Wallace <bencwallace@gmail.com>
2024-05-23 14:23:46 +0200

    add walk_tree tests

---
+++ tests/walk_tree_test.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+
+#include "walk_tree.h"
+
+using namespace pivot;
+
+TEST(WalkTreeInit, Line) {
+    walk_tree *w = walk_tree::line(5);
+    ASSERT_TRUE(w->self_avoiding());
+    auto steps = w->steps();
+    EXPECT_EQ(steps.size(), 5);
+    for (int i = 0; i < 5; i++) {
+        EXPECT_EQ(steps[i], pivot::point(i + 1, 0));
+    }
+    EXPECT_FALSE(w->is_leaf());
+    EXPECT_EQ(w->endpoint(), pivot::point(5, 0));
+    delete w;
+}
+
+TEST(WalkTreePivot, PivotLine) {
+    walk_tree *w = walk_tree::line(2);
+    EXPECT_TRUE(w->try_pivot(1, angle::ninety));
+    auto steps = w->steps();
+    EXPECT_EQ(steps.size(), 2);
+    EXPECT_EQ(steps[0], pivot::point(1, 0));
+    EXPECT_EQ(steps[1], pivot::point(1, 1));
+    delete w;
+}
+
+TEST(WalkTreePivot, PivotLineFail) {
+    walk_tree *w = walk_tree::line(3);
+    EXPECT_FALSE(w->try_pivot(2, angle::one_eighty));
+    auto steps = w->steps();
+    EXPECT_EQ(steps.size(), 3);
+    EXPECT_EQ(steps[0], pivot::point(1, 0));
+    EXPECT_EQ(steps[1], pivot::point(2, 0));
+    EXPECT_EQ(steps[2], pivot::point(3, 0));
+    delete w;
+}
