a43a28480379e50870e33cbd466d2724a18cac47
Ben Wallace <bencwallace@gmail.com>
2024-05-27 13:29:22 +0200

    templatize main loop

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,17 @@
 #include "walk_base.h"
 #include "walk_tree.h"
 
-int main(int argc, char **argv) {
-  int num_steps;
-  int iters;
-  bool naive{false};
-  int num_workers{0};
-  bool require_success{false};
-  bool verify{false};
-  std::string out_dir{""};
-  int seed;
-
-  CLI::App app{"Implementation of the pivot algorithm"};
-  argv = app.ensure_utf8(argv);
-
-  app.add_option("-s,--steps", num_steps, "number of steps")->required();
-  app.add_option("-i,--iters", iters, "number of iterations")->required();
-  app.add_flag("--naive", naive, "use naive implementation (slower)");
-  app.add_option("-w,--workers", num_workers, "number of workers");
-  app.add_flag("--success", require_success, "require success");
-  app.add_flag("--verify", verify, "verify");
-  app.add_option("--out", out_dir, "output directory");
-  app.add_option("--seed", seed, "seed")->default_val(time(nullptr));
-
-  CLI11_PARSE(app, argc, argv);
-
-  pivot::walk_base<2> *w;
+template <int Dim>
+int main_loop(int num_steps, int iters, bool naive, int seed, bool require_success, bool verify,
+              const std::string &out_dir) {
+  pivot::walk_base<Dim> *w;
   if (!naive) {
-    w = pivot::walk_tree<2>::line(num_steps);
+    w = pivot::walk_tree<Dim>::line(num_steps);
   } else {
-    w = new pivot::walk<2>(num_steps);
+    w = new pivot::walk<Dim>(num_steps);
   }
 
-  std::vector<pivot::point<2>> endpoints;
+  std::vector<pivot::point<Dim>> endpoints;
   if (require_success) {
     endpoints.reserve(iters);
   }
@@ -80,4 +59,50 @@ int main(int argc, char **argv) {
     }
   }
   delete w;
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int dim;
+  int num_steps;
+  int iters;
+  bool naive{false};
+  int num_workers{0};
+  bool require_success{false};
+  bool verify{false};
+  std::string out_dir{""};
+  int seed;
+
+  CLI::App app{"Implementation of the pivot algorithm"};
+  argv = app.ensure_utf8(argv);
+
+  app.add_option("-d,--dim", dim, "dimension")->required();
+  app.add_option("-s,--steps", num_steps, "number of steps")->required();
+  app.add_option("-i,--iters", iters, "number of iterations")->required();
+  app.add_flag("--naive", naive, "use naive implementation (slower)");
+  app.add_option("-w,--workers", num_workers, "number of workers");
+  app.add_flag("--success", require_success, "require success");
+  app.add_flag("--verify", verify, "verify");
+  app.add_option("--out", out_dir, "output directory");
+  app.add_option("--seed", seed, "seed")->default_val(time(nullptr));
+
+  CLI11_PARSE(app, argc, argv);
+
+  switch (dim) {
+  case 2:
+    return main_loop<2>(num_steps, iters, naive, seed, require_success, verify, out_dir);
+    break;
+  case 3:
+    return main_loop<3>(num_steps, iters, naive, seed, require_success, verify, out_dir);
+    break;
+  case 4:
+    return main_loop<4>(num_steps, iters, naive, seed, require_success, verify, out_dir);
+    break;
+  case 5:
+    return main_loop<5>(num_steps, iters, naive, seed, require_success, verify, out_dir);
+    break;
+  default:
+    std::cerr << "Invalid dimension: " << dim << '\n';
+    return 1;
+  }
 }
