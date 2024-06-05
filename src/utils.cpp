528dfa19fd756a6ecb8c0c39c7b8bb815dddec16
Ben Wallace <bencwallace@gmail.com>
2024-06-05 23:50:30 +0200

    circumvent gcc-12 string concatenation issue

--- src/utils.cpp
+++ src/utils.cpp
@@ -19,6 +19,11 @@ bool interval::operator!=(const interval &other) const { return !(*this == other
 bool interval::empty() const { return left_ > right_; }
 
 /** @brief Returns the string of the form "[{left_}, {right_}]". */
-std::string interval::to_string() const { return "[" + std::to_string(left_) + ", " + std::to_string(right_) + "]"; }
+std::string interval::to_string() const {
+  // for some reason, gcc-12 gives -Wrestrict warning if I use the string concatenation operator
+  std::string result = "[";
+  result.append(std::to_string(left_)).append(", ").append(std::to_string(right_)).append("]");
+  return result;
+}
 
 } // namespace pivot
