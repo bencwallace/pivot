0237e1e6ef7197c01d43074afb5eb34a35564bf8
Ben Wallace <bencwallace@gmail.com>
2024-06-13 10:41:57 +0200

    test self-avoidance over range of walk sizes

--- tests/int_test.cpp
+++ tests/int_test.cpp
@@ -5,22 +5,26 @@
 
 using namespace pivot;
 
-TEST(IntTest, Walk) {
-  walk<2> w(100);
-  for (int i = 0; i < 10; ++i) {
-    for (int j = 0; j < 100; j++) {
-      w.rand_pivot();
+TEST(WalkTest, SelfAvoiding) {
+  for (int num_steps = 2; num_steps < 10; ++num_steps) {
+    walk<2> w(100);
+    for (int i = 0; i < 10; ++i) {
+      for (int j = 0; j < 100; j++) {
+        w.rand_pivot();
+      }
+      EXPECT_TRUE(w.self_avoiding());
     }
-    EXPECT_TRUE(w.self_avoiding());
   }
 }
 
-TEST(IntTest, WalkTree) {
-  auto w = walk_tree<2>(2);
-  for (int i = 0; i < 10; ++i) {
-    for (int j = 0; j < 100; j++) {
-      w.rand_pivot();
+TEST(WalkTreeTest, SelfAvoiding) {
+  for (int num_steps = 2; num_steps < 10; ++num_steps) {
+    auto w = walk_tree<2>(2);
+    for (int i = 0; i < 10; ++i) {
+      for (int j = 0; j < 100; j++) {
+        w.rand_pivot();
+      }
+      EXPECT_TRUE(w.self_avoiding());
     }
-    EXPECT_TRUE(w.self_avoiding());
   }
 }
