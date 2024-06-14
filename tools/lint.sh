688dc2e486a2fcbf63d0c24b8e0ba5e5e29c69fe
Ben Wallace <bencwallace@gmail.com>
2024-06-14 18:46:16 +0200

    use cmake presets in CI/CD

--- tools/lint.sh
+++ tools/lint.sh
@@ -15,6 +15,6 @@ cppcheck \
     --enable=warning,performance,information,missingInclude \
     --error-exitcode=1 \
     --suppress=missingIncludeSystem \
-    -i build \
-    --project=build/compile_commands.json
-find src \( -name *.cpp -o -name *.h \) -print0 | xargs -0 -n1 clang-tidy-14 -p build ${ARGS}
+    -i build/$1 \
+    --project=build/$1/compile_commands.json
+find src \( -name *.cpp -o -name *.h \) -print0 | xargs -0 -n1 clang-tidy-14 -p build/$1 ${ARGS}
