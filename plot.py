225b88c69cfdf8e28d5ad11d09c51727dc9650fa
Ben Wallace <bencwallace@gmail.com>
2024-07-26 17:22:56 +0200

    add (interactive) plot to readme

--- plot.py
+++ plot.py
@@ -1,17 +1,22 @@
 import csv
 import sys
 
-import matplotlib.pyplot as plt
 
-
-def main(path):
+def main(path, use_plotly=False):
     with open(path) as f:
         reader = csv.reader(f)
         data = [[int(y) for y in x] for x in reader]
 
-    plt.plot(*zip(*data))
-    plt.show()
+    x, y = zip(*data)
+    if use_plotly:
+        import plotly.graph_objects as go
+        fig = go.Figure(data=go.Scatter(x=x, y=y))
+        fig.show()
+    else:
+        import matplotlib.pyplot as plt
+        plt.plot(x, y)
+        plt.show()
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else False)
