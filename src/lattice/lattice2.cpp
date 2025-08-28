8acf03eb1332c76d9379ca703dfe799d187b2c99
Ben Wallace <bencwallace@gmail.com>
2025-08-28 14:48:25 +0200

    Specialize transform<2> with SIMD optimization

--- src/lattice/lattice2.cpp
+++ src/lattice/lattice2.cpp
@@ -32,12 +32,25 @@ static inline __m128i insert_epi32(__m128i data, int val, size_t i) {
   }
 }
 
+inline __m128i permutevar_epi32(__m128i data, __m128i perm) {
+  return _mm_castps_si128(_mm_permutevar_ps(_mm_castsi128_ps(data), perm));
+}
+
+inline __m128i sort_bounds(__m128i pairs) {
+  __m128i swapped = _mm_shuffle_epi32(pairs, _MM_SHUFFLE(1, 0, 3, 2));
+  __m128i minima = _mm_min_epi32(pairs, swapped);
+  __m128i maxima = _mm_max_epi32(pairs, swapped);
+  return _mm_blend_epi32(minima, maxima, 0b1100);
+}
+
 namespace pivot {
 
 template <> class point<2> : boost::multipliable<point<2>, int> {
 
 public:
   point() : coords_(_mm_setzero_si128()) {}
+
+  point(__m128i coords) : coords_(coords) {}
 
   point(const std::array<int, 2> &coords) : coords_(_mm_setr_epi32(coords[0], coords[1], 0, 0)) {}
 
@@ -80,12 +93,12 @@ public:
 
 private:
   __m128i coords_;
-
-  point(__m128i coords) : coords_(coords) {}
 };
 
 template <> struct box<2> : boost::additive<box<2>, point<2>> {
   box() = delete;
+
+  box(__m128i data) : data_(data) {}
 
   box(const std::array<interval, 2> &intervals)
       : data_(_mm_setr_epi32(intervals[0].left_, intervals[1].left_, intervals[0].right_, intervals[1].right_)) {}
@@ -113,6 +126,8 @@ template <> struct box<2> : boost::additive<box<2>, point<2>> {
   bool operator!=(const box &b) const { return !(*this == b); }
 
   interval operator[](int i) const { return {int32_t(extract_epi32(data_, i)), int32_t(extract_epi32(data_, i + 2))}; }
+
+  __m128i data() const { return data_; }
 
   bool empty() const {
     __m128i swapped = _mm_shuffle_epi32(data_, _MM_SHUFFLE(1, 0, 3, 2));
@@ -157,8 +172,113 @@ template <> struct box<2> : boost::additive<box<2>, point<2>> {
 
 private:
   __m128i data_; // x_min, y_min, x_max, y_max
+};
 
-  box(__m128i data) : data_(data) {}
+template <> class transform<2> {
+
+public:
+  transform() : perm_(_mm_setr_epi32(0, 1, 2, 3)), signs_(_mm_set1_epi32(1)) {}
+
+  transform(const std::array<uint32_t, 2> &perm, const std::array<int32_t, 2> &signs)
+      : perm_(_mm_setr_epi32(perm[0], perm[1], 2 + perm[0], 2 + perm[1])),
+        signs_(_mm_setr_epi32(signs[0], signs[1], signs[0], signs[1])) {}
+
+  transform(const point<2> &p, const point<2> &q) : transform() {
+    point diff = q - p;
+    int idx = -1;
+    for (int i = 0; i < 2; ++i) {
+      if (std::abs(diff[i]) == 1) {
+        if (idx == -1) {
+          idx = i;
+        } else {
+          throw std::invalid_argument("Points are not adjacent");
+        }
+      }
+    }
+    if (idx == -1) {
+      throw std::invalid_argument("Points are not adjacent");
+    }
+
+    insert_epi32(perm_, idx, 0);
+    insert_epi32(perm_, 0, idx);
+    insert_epi32(signs_, -diff[idx], 0);
+    insert_epi32(signs_, diff[idx], idx);
+  }
+
+  template <typename Gen> static transform rand(Gen &gen) {
+    // TODO: Can this be vectorized?
+    static std::bernoulli_distribution flip_;
+
+    std::array<uint32_t, 2> perm = {0, 1};
+    std::array<int32_t, 2> signs;
+    for (int i = 0; i < 2; ++i) {
+      signs[i] = 2 * flip_(gen) - 1;
+    }
+    std::shuffle(perm.begin(), perm.end(), gen);
+    return transform(perm, signs);
+  }
+
+  static transform rand() {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    return rand(gen);
+  }
+
+  bool operator==(const transform &t) const {
+    return _mm_movemask_epi8(_mm_cmpeq_epi32(signs_, t.signs_)) == 0xFFFF &&
+           _mm_movemask_epi8(_mm_cmpeq_epi32(perm_, t.perm_)) == 0xFFFF;
+  }
+
+  point<2> operator*(const point<2> &p) const { return _mm_sign_epi32(permutevar_epi32(p.coords(), perm_), signs_); }
+
+  transform operator*(const transform &t) const {
+    __m128i perm = permutevar_epi32(perm_, t.perm_);
+    __m128i signs = _mm_sign_epi32(permutevar_epi32(t.signs_, perm_), signs_);
+    return {signs, perm};
+  }
+
+  box<2> operator*(const box<2> &b) const {
+    __m128i pairs = _mm_sign_epi32(permutevar_epi32(b.data(), perm_), signs_);
+    return sort_bounds(pairs);
+  }
+
+  bool is_identity() const {
+    return _mm_movemask_epi8(_mm_cmpeq_epi32(signs_, _mm_set1_epi32(1))) == 0xFFFF &&
+           _mm_movemask_epi8(_mm_cmpeq_epi32(perm_, _mm_setr_epi32(0, 1, 2, 3))) == 0xFFFF;
+  }
+
+  transform inverse() const {
+    // In general, the inverse is given by signs S' and permutations P' such
+    // that P' = P^-1 and S' = S P. The latter's components can be obtained by
+    // viewing S as a vector and applying P to it (i.e. permuting it). Moreover,
+    // in 2D, P^-1 is the same as P.
+    return {perm_, permutevar_epi32(signs_, perm_)};
+  }
+
+  std::array<std::array<int, 2>, 2> to_matrix() const {
+    std::array<std::array<int, 2>, 2> mat;
+    mat[perm_[0]][0] = extract_epi32(signs_, extract_epi32(perm_, 0));
+    mat[perm_[1]][1] = extract_epi32(signs_, extract_epi32(perm_, 1));
+    return mat;
+  }
+
+  std::string to_string() const {
+    auto matrix = to_matrix();
+    std::string s = "[";
+    for (int i = 0; i < 2; ++i) {
+      s += "[";
+      s += std::to_string(matrix[i][0]) + ", ";
+      s += std::to_string(matrix[i][1]) + "]";
+      if (i < 1) {
+        s += ", ";
+      }
+    }
+    return s;
+  }
+
+private:
+  __m128i perm_;
+  __m128i signs_;
 };
 
 } // namespace pivot
