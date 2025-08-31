3640ba9e9f91678fa30b7969ba25f130e67d3d2b
Ben Wallace <bencwallace@gmail.com>
2025-08-31 13:29:11 +0200

    Extend point to include Simd policy

--- src/lattice/point.hpp
+++ src/lattice/point.hpp
@@ -4,17 +4,17 @@
 
 namespace pivot {
 
-template <int Dim> point<Dim>::point(const std::array<int, Dim> &coords) : coords_(coords) {}
+template <int Dim, bool Simd> point<Dim, Simd>::point(const std::array<int, Dim> &coords) : coords_(coords) {}
 
-template <int Dim> point<Dim> point<Dim>::unit(int i) {
-  point<Dim> result;
+template <int Dim, bool Simd> point<Dim, Simd> point<Dim, Simd>::unit(int i) {
+  point<Dim, Simd> result;
   result.coords_[i] = 1;
   return result;
 }
 
-template <int Dim> int point<Dim>::operator[](int i) const { return coords_[i]; }
+template <int Dim, bool Simd> int point<Dim, Simd>::operator[](int i) const { return coords_[i]; }
 
-template <int Dim> bool point<Dim>::operator==(const point &p) const {
+template <int Dim, bool Simd> bool point<Dim, Simd>::operator==(const point &p) const {
   // Faster than using std::equal, at least for small arrays
   // https://stackoverflow.com/questions/39262496/why-is-stdequal-much-slower-than-a-hand-rolled-loop-for-two-small-stdarray
   for (int i = 0; i < Dim; ++i) {
@@ -25,9 +25,9 @@ template <int Dim> bool point<Dim>::operator==(const point &p) const {
   return true;
 }
 
-template <int Dim> bool point<Dim>::operator!=(const point &p) const { return !(*this == p); }
+template <int Dim, bool Simd> bool point<Dim, Simd>::operator!=(const point &p) const { return !(*this == p); }
 
-template <int Dim> point<Dim> point<Dim>::operator+(const point<Dim> &p) const {
+template <int Dim, bool Simd> point<Dim, Simd> point<Dim, Simd>::operator+(const point<Dim, Simd> &p) const {
   std::array<int, Dim> sum;
   for (int i = 0; i < Dim; ++i) {
     sum[i] = coords_[i] + p.coords_[i];
@@ -35,7 +35,7 @@ template <int Dim> point<Dim> point<Dim>::operator+(const point<Dim> &p) const {
   return point(sum);
 }
 
-template <int Dim> point<Dim> point<Dim>::operator-(const point &p) const {
+template <int Dim, bool Simd> point<Dim, Simd> point<Dim, Simd>::operator-(const point &p) const {
   std::array<int, Dim> diff;
   for (int i = 0; i < Dim; ++i) {
     diff[i] = coords_[i] - p.coords_[i];
@@ -43,14 +43,14 @@ template <int Dim> point<Dim> point<Dim>::operator-(const point &p) const {
   return point(diff);
 }
 
-template <int Dim> point<Dim> &point<Dim>::operator*=(int k) {
+template <int Dim, bool Simd> point<Dim, Simd> &point<Dim, Simd>::operator*=(int k) {
   for (int i = 0; i < Dim; ++i) {
     coords_[i] *= k;
   }
   return *this;
 }
 
-template <int Dim> int point<Dim>::norm() const {
+template <int Dim, bool Simd> int point<Dim, Simd>::norm() const {
   int sum = 0;
   for (int i = 0; i < Dim; ++i) {
     sum += coords_[i] * coords_[i];
@@ -58,7 +58,7 @@ template <int Dim> int point<Dim>::norm() const {
   return sum;
 }
 
-template <int Dim> std::string point<Dim>::to_string() const {
+template <int Dim, bool Simd> std::string point<Dim, Simd>::to_string() const {
   std::string s = "(";
   for (int i = 0; i < Dim - 1; ++i) {
     s += std::to_string(coords_[i]) + ", ";
