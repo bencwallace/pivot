40827fc84798f4e6318d595c2cff0f0154ddd836
Ben Wallace <bencwallace@gmail.com>
2025-08-31 13:38:48 +0200

    Extend transform to include Simd policy

--- src/lattice/transform.hpp
+++ src/lattice/transform.hpp
@@ -4,20 +4,21 @@
 
 namespace pivot {
 
-template <int Dim> transform<Dim>::transform() {
+template <int Dim, bool Simd> transform<Dim, Simd>::transform() {
   for (int i = 0; i < Dim; ++i) {
     perm_[i] = i;  // trivial permutation
     signs_[i] = 1; // standard orientations (no flips)
   }
 }
 
-template <int Dim>
-transform<Dim>::transform(const std::array<int, Dim> &perm, const std::array<int, Dim> &signs)
+template <int Dim, bool Simd>
+transform<Dim, Simd>::transform(const std::array<int, Dim> &perm, const std::array<int, Dim> &signs)
     : perm_(perm), signs_(signs) {}
 
-template <int Dim> transform<Dim>::transform(const point<Dim> &p, const point<Dim> &q) : transform() {
+template <int Dim, bool Simd>
+transform<Dim, Simd>::transform(const point<Dim, Simd> &p, const point<Dim, Simd> &q) : transform() {
   // The input points should differ by 1 in a single coordinate. Start by finding this coordinate or fail.
-  point<Dim> diff = q - p;
+  point<Dim, Simd> diff = q - p;
   int idx = -1;
   for (int i = 0; i < Dim; ++i) {
     if (std::abs(diff[i]) == 1) {
@@ -39,25 +40,26 @@ template <int Dim> transform<Dim>::transform(const point<Dim> &p, const point<Di
   signs_[idx] = diff[idx];
 }
 
-template <int Dim> transform<Dim> transform<Dim>::rand() {
+template <int Dim, bool Simd> transform<Dim, Simd> transform<Dim, Simd>::rand() {
   static std::random_device rd;
   static std::mt19937 gen(rd());
   return rand(gen);
 }
 
-template <int Dim> bool transform<Dim>::operator==(const transform &t) const {
+template <int Dim, bool Simd> bool transform<Dim, Simd>::operator==(const transform &t) const {
   return perm_ == t.perm_ && signs_ == t.signs_;
 }
 
-template <int Dim> point<Dim> transform<Dim>::operator*(const point<Dim> &p) const {
+template <int Dim, bool Simd> point<Dim, Simd> transform<Dim, Simd>::operator*(const point<Dim, Simd> &p) const {
   std::array<int, Dim> coords;
   for (int i = 0; i < Dim; ++i) {
     coords[perm_[i]] = signs_[perm_[i]] * p[i];
   }
-  return point<Dim>(coords);
+  return point<Dim, Simd>(coords);
 }
 
-template <int Dim> transform<Dim> transform<Dim>::operator*(const transform<Dim> &t) const {
+template <int Dim, bool Simd>
+transform<Dim, Simd> transform<Dim, Simd>::operator*(const transform<Dim, Simd> &t) const {
   std::array<int, Dim> perm;
   std::array<int, Dim> signs;
   for (int i = 0; i < Dim; ++i) {
@@ -67,17 +69,17 @@ template <int Dim> transform<Dim> transform<Dim>::operator*(const transform<Dim>
   return transform(perm, signs);
 }
 
-template <int Dim> box<Dim> transform<Dim>::operator*(const box<Dim> &b) const {
+template <int Dim, bool Simd> box<Dim, Simd> transform<Dim, Simd>::operator*(const box<Dim, Simd> &b) const {
   std::array<interval, Dim> intervals;
   for (int i = 0; i < Dim; ++i) {
     int x = signs_[perm_[i]] * b.intervals_[i].left_;
     int y = signs_[perm_[i]] * b.intervals_[i].right_;
     intervals[perm_[i]] = interval(std::min(x, y), std::max(x, y));
   }
-  return box<Dim>(intervals);
+  return box<Dim, Simd>(intervals);
 }
 
-template <int Dim> bool transform<Dim>::is_identity() const {
+template <int Dim, bool Simd> bool transform<Dim, Simd>::is_identity() const {
   for (int i = 0; i < Dim; ++i) {
     if (perm_[i] != i || signs_[i] != 1) {
       return false;
@@ -86,7 +88,7 @@ template <int Dim> bool transform<Dim>::is_identity() const {
   return true;
 }
 
-template <int Dim> transform<Dim> transform<Dim>::inverse() const {
+template <int Dim, bool Simd> transform<Dim, Simd> transform<Dim, Simd>::inverse() const {
   std::array<int, Dim> perm;
   std::array<int, Dim> signs;
   for (int i = 0; i < Dim; ++i) {
@@ -96,7 +98,7 @@ template <int Dim> transform<Dim> transform<Dim>::inverse() const {
   return transform(perm, signs);
 }
 
-template <int Dim> std::array<std::array<int, Dim>, Dim> transform<Dim>::to_matrix() const {
+template <int Dim, bool Simd> std::array<std::array<int, Dim>, Dim> transform<Dim, Simd>::to_matrix() const {
   std::array<std::array<int, Dim>, Dim> matrix = {};
   for (int i = 0; i < Dim; ++i) {
     matrix[perm_[i]][i] = signs_[perm_[i]];
@@ -104,7 +106,7 @@ template <int Dim> std::array<std::array<int, Dim>, Dim> transform<Dim>::to_matr
   return matrix;
 }
 
-template <int Dim> std::string transform<Dim>::to_string() const {
+template <int Dim, bool Simd> std::string transform<Dim, Simd>::to_string() const {
   auto matrix = to_matrix();
   std::string s = "[";
   for (int i = 0; i < Dim; ++i) {
