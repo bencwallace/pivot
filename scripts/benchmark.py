fc61be9e5b5a214640ee08136451082fecdae040
Ben Wallace <bencwallace@gmail.com>
2025-10-31 18:26:12 +0100

    Extend benchmark script to support naive algorithm

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -30,20 +30,34 @@ def warmup(dim: int, seed: int | None = None):
         print(f"Checkpoint saved to {out_dir}/walk.csv")
 
 
-def benchmark(dim: int, slow: bool, seed: int | None = None):
+def benchmark(dim: int, slow: bool, naive: bool = False, seed: int | None = None):
+    if naive:
+        slow = True
     print(f"Running benchmark for dimension {dim}")
     times = {}
     for steps in STEPS:
         print(f"Running benchmark with {steps} steps for {BENCH_ITERS} iterations")
 
         in_dir = Path(__file__).parent / "benchmark" / f"dim_{dim}" / f"warmup_{steps}/walk.csv"
-        cmd = f"{pivot_path} -d {dim} -s {steps} -i {BENCH_ITERS} --in {in_dir} --{'slow' if slow else 'fast'} "
+        cmd = (
+            f"{pivot_path} "
+            f"-d {dim} "
+            f"-s {steps} "
+            f"-i {BENCH_ITERS} "
+            f"--in {in_dir} "
+            f"--{'slow' if slow else 'fast'} "
+            f"{'--naive' if naive else ''} "
+        )
         if seed is not None:
             cmd += f"--seed {seed}"
         proc = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
         proc.stderr.readline()
         start = time.time()
         proc.wait()
+        if proc.returncode != 0:
+            print(cmd)
+            print(proc.stderr.read().decode())
+            raise RuntimeError(f"Benchmark failed with return code {proc.returncode}")
         stop = time.time()
         times[steps] = 1_000_000 * (stop - start) / BENCH_ITERS
         print(f"Time per pivot attempt: {times[steps]:.2f} µs")
@@ -78,6 +92,7 @@ if __name__ == "__main__":
     benchmark_parser = subparsers.add_parser("benchmark")
     benchmark_parser.add_argument("--slow", default=False, action="store_true")
     benchmark_parser.add_argument("--seed", default=None)
+    benchmark_parser.add_argument("--naive", default=False, action="store_true")
 
     analyze_parser = subparsers.add_parser("analyze")
 
@@ -85,6 +100,6 @@ if __name__ == "__main__":
     if args.command == "warmup":
         warmup(args.dim, args.seed)
     elif args.command == "benchmark":
-        benchmark(args.dim, args.slow, args.seed)
+        benchmark(args.dim, args.slow, naive=args.naive, seed=args.seed)
     elif args.command == "analyze":
         analyze(args.dim)
